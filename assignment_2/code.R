655f4a8bed83f7a820e098cbcc5b4e929e30a43d
hanman453 <81697653+hanman453@users.noreply.github.com>
2025-02-27 11:55:18 +0000

    correlations

--- assignment_2/code.R
+++ assignment_2/code.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/BCapo/Desktop/University of Edinburgh Masters/Sem 2/srs-assignment/assignment_2")
+setwd("/Users/cyrusseyrafi/srs-assignment/assignment_2")
 
 # NOTES #
 # Q1 is the 20% of areas with lowest participation in higher education
@@ -23,6 +23,21 @@ data = data[,-SIMD.cols]
 # Reset the cols data. 
 cols = as.list(1:ncol(data))
 names(cols) = names(data)
+
+# Initialize correlations.csv
+correlations <- cor(numeric_data, use = "complete.obs")
+
+# Convert correlations to a data frame
+cor_df <- as.data.frame(correlations)
+cor_df <- cbind(Variable = rownames(cor_df), cor_df)
+rownames(cor_df) <- NULL
+
+cor_df <- data.frame(cor_df)
+# Save correlations to a CSV file
+write.csv(cor_df, "correlations.csv", row.names = FALSE)
+
+# Print results
+print(cor_df)
 
 # EDA #
 
